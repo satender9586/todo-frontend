6f49fa0bc90a6d17567daf5c59edcbf62c0af2c8
satender <satender@paytelgroup.com>
2025-02-06 17:50:00 +0530

    protect route fixed

--- src/lib/ProtectRoutes.tsx
+++ src/lib/ProtectRoutes.tsx
@@ -1,51 +1,51 @@
-import { PUBLIC_ROUTES,PRIVATE_ROUTES,DEFAULT_ROUTE } from "../constant/ProtectedRouteData";
+import { PUBLIC_ROUTES, PRIVATE_ROUTES, DEFAULT_ROUTE } from "../constant/ProtectedRouteData";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { UserInterface } from "../constant/TypeNotes";
 
-
-
-
-
 type Props = {
-  children: React.ReactNode; 
+  children: React.ReactNode;
 };
 
-
 export const ProtectedRoutes = ({ children }: Props) => {
-    const navigate = useNavigate();
-    const userReduxData = useSelector((state: { auth: UserInterface }) => state.auth);
-    const location = useLocation();
-    const isAuthenticated: boolean = userReduxData?.isAuthenticated ?? false;
-    const token: string | null = userReduxData?.userToken ?? null;
-    const currentPath = location.pathname;
-    const isBrowser = typeof window !== "undefined";
+  const navigate = useNavigate();
+  const userReduxData = useSelector((state: { auth: UserInterface }) => state.auth);
+  const location = useLocation();
+  
+  const isAuthenticated = userReduxData?.isAuthenticated ?? false;
+  const token = userReduxData?.userToken ?? null;
+  const currentPath = location.pathname;
 
+  const isBrowser = typeof window !== "undefined";
+
+
+  const isProtectedRoute = Object.values(PRIVATE_ROUTES).includes(currentPath);
+  const isPublicRoute = Object.values(PUBLIC_ROUTES).includes(currentPath);
+  const isDefaultRoute = Object.values(DEFAULT_ROUTE).includes(currentPath);
+
+
+  const checkRoutes = () => {
    
-    const checkRoutes = ()=>{
-    
-        const isProtectedRoute = Object.values(PRIVATE_ROUTES).includes(currentPath)
-        const isPublicRoute = Object.values(PUBLIC_ROUTES).includes(currentPath)
-        const isDefaultRoute = Object.values(DEFAULT_ROUTE).includes(currentPath)
-
-        if(isProtectedRoute && isAuthenticated && token){
-           const path: string = currentPath || "/dashboard";
-           navigate(path);
-        } if(isPublicRoute ||  isDefaultRoute  && ((!isAuthenticated && !token))){
-            navigate(currentPath)
-        }else{
-            navigate("/")
-        }
+    if (isProtectedRoute && (!isAuthenticated || !token)) {
+      navigate("/"); 
+      return;
     }
 
-    useEffect(() => {
-        if (isBrowser) {
-          checkRoutes(); 
-        }
-      }, [location.pathname,isAuthenticated]);
- 
+
+    if (isPublicRoute || isDefaultRoute) {
+      if (isAuthenticated && token) {
+        navigate("/dashboard"); 
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isBrowser) {
+      checkRoutes();
+    }
+  }, [location.pathname, isAuthenticated, token]);
 
   return <>{children}</>;
 };
