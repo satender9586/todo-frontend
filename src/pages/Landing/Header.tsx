5ed64b7c7cad90156376b3e5ca4ef7b5291b8da2
satender <satender@paytelgroup.com>
2025-02-06 10:36:14 +0530

    footer address change

--- src/pages/Landing/Header.tsx
+++ src/pages/Landing/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
           </h1>
           <h1 className=" sm:mt-0 md:mt-2 lg:mt-[1rem] text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-medium leading-tigh"> Your Notes</h1>
           <span className="sm:mt-1 md:mt-2 block text-xl md:text-2xl text-[#CB76BA]">Stay organized, stay productive.</span>
-          <p className="text-[17px] sm:mt-1 md:mt-2 opacity-[0.8]">Capture thoughts, ideas, and tasks with ease. Your notes,</p>
+          <p className="text-[17px] sm:mt-1 md:mt-2 opacity-[0.8]">Capture thoughts, ideas, and tasks with easily. Your notes,</p>
           <p className="text-[17px] sm:mt-[1] md:mt-1 opacity-[0.8]">always within reach., always within reach.</p>
           <button onClick={()=> navigate("/login")} className="mt-[1rem] rounded-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6  text-md">
             Get Started
